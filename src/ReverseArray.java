40521d8b446c397eb3a2552e11405d97ab5453a4
Yash Gajjar <gajjaryash9999@gmail.com>
2025-02-11 23:57:06 +0530

    updating ReverseArray

--- src/ReverseArray.java
+++ src/ReverseArray.java
@@ -2,20 +2,29 @@ import java.util.Scanner;
 
 public class ReverseArray {
 
-    public static void ReverseNumbers(int[] numbers) {
-        int[] reversedNumbers = new int[10];
-        int traverse = 0;
+//    public static void ReverseNumbers(int[] numbers) {
+//        int[] reversedNumbers = new int[10];
+//        int traverse = 0;
+//        // for reversing an array.
+//        for ( int i = (numbers.length-1);  i >= 0 ;  i-- ){
+//            reversedNumbers[i] = numbers[traverse];
+//            traverse++;
+//        }
+//        // for printing a reversed array.
+//        for ( int i = 0 ; i < reversedNumbers.length ; i ++) {
+//            System.out.print(reversedNumbers[i] + " ");
+//        }
+//    }
+
+
+// Easy Method
+public static void ReverseNumbers(int[] numbers) {
         // for reversing an array.
         for ( int i = (numbers.length-1);  i >= 0 ;  i-- ){
-            reversedNumbers[i] = numbers[traverse];
-            traverse++;
+            System.out.print(numbers[i] + " ");
         }
-        // for printing a reversed array.
-        for ( int i = 0 ; i < reversedNumbers.length ; i ++) {
-            System.out.print(reversedNumbers[i] + " ");
-        }
-    }
 
+    }
     public static void main(String[] args) {
         int[] arrayNumbers = new int[10];
         Scanner sc = new Scanner(System.in);
